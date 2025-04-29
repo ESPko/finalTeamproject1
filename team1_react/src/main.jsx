7ba0f507a1a66c4eec6e9fb32d25ea9e81353c0d
ESPko <asddsa362@naver.com>
2025-04-29 09:11:23 +0900

    로그인페이지 숨기고 메인페이지 확설화

--- team1_react/src/main.jsx
+++ team1_react/src/main.jsx
@@ -1,20 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Header from './layout/Header.jsx';
 import SideBar from './layout/SideBar.jsx';
-import Topline from './layout/Topline.jsx';
 import Login from './components/login/Login.jsx';
 import Login2 from './components/login/Login2.jsx';
 import Login3 from './components/login/Login3.jsx';
 import EquipmentInformation from './page/EquipmentInformation.jsx';
+import ProductList from './page/ProductList/ProductList.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
-    <Login2 />
-    <Login3 />
+    {/*<Login />*/}
+    {/*<Login2 />*/}
+    {/*<Login3 />*/}
     {/*<App />*/}
     <Header />
     <div className="d-flex">
