76df7c1bd9fdb63254e26867748430b3c65841b0
donghoon1634 <tkdduq010@naver.com>
2025-04-29 16:20:02 +0900

    비품 목록 페이지 더미 데이터 넣어서 완료
    NoResultProduct.jsx
    - 재고 수량이 아에 없거나 해당 제품이 존재하지 않을 경우 나타나는 컴포넌트

--- team1_react/src/page/ProductList/ProductDetail.jsx
+++ team1_react/src/page/ProductList/ProductDetail.jsx
@@ -1,40 +1,37 @@
-function productDetail() {
+function productDetail({product}) {
+
+  if (!product) return null;
+
   return (
     <div >
-      <div className={'border'}>
-    <div className={'d-flex m-3'}>
-      <img src="src/productImg/pencil.png" width={'50px'} height={'50px'} className={'me-3'}/>
+      <div className="border rounded ">
+        <div className="flex items-center m-3">
+          <img src="src/productImg/pencil.png"
+              className="w-[50px] h-[50px] mr-3"/>
 
-      <div className={'d-grid'}>
-        <p>비품 5</p>
-        <p>적정 수랑</p>
-      </div>
-      <div className={'ms-auto p-3'}>
-        <p style={{ color: '#7b68ee' }}>현재 수량</p>
-      </div>
-    </div>
-    </div>
-      <div style={{background: 'lightGray'}}>재고 위치</div>
-    <div className={'border'}>
-      <ul>
-        <li>
-          <div className={'d-flex mt-2'}>
-          <p>창고1</p>
-            <div className={'ms-auto me-3'}>
-          <p style={{ color: '#7b68ee' }}>수량</p>
-            </div>
+          <div className="flex flex-col">
+            <p className="text-lg font-medium">{product.name}</p>
+            <p className="text-sm text-gray-500">적정 수랑: {product.standardStock}</p>
           </div>
-        </li>
-        <li>
-          <div className={'d-flex mt-2'}>
-            <p>창고2</p>
-            <div className={'ms-auto me-3'}>
-              <p style={{ color: '#7b68ee' }}>수량</p>
-            </div>
+          <div className="ml-auto p-3">
+            <p className="text-indigo-500 font-semibold">{product.amount}</p>
           </div>
-        </li>
-      </ul>
-    </div>
+        </div>
+      </div>
+      <div className="border rounded">
+        <div className="bg-gray-200 px-4 py-2">
+          <h6 className="text-base font-semibold">재고 위치</h6>
+        </div>
+        <ul>
+          <li>
+            <div className="flex items-center justify-between px-4 py-2">
+              <p>{product.location}</p>
+
+                <p className="text-indigo-500">{product.amount} 개</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
