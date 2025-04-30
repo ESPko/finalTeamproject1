5ada0ff7bd38532977bfdd8dcf0b1a1564035345
jungg <yjungggg@gmail.com>
2025-04-30 11:11:16 +0900

    no message

---
+++ team1_react/src/components/layout/Storage.jsx
@@ -0,0 +1,11 @@
+function Storage() {
+  return (
+    <div className={'container m-0 p-0'} style={{height: '1530px', width: '705px'}}>
+    <div className={'d-flex align-items-end'} style={{borderBottom : "1px solid black" }}>
+      <div><h4>입고</h4></div>
+    </div>
+    </div>
+  );
+}
+
+export default Storage;
--- team1_react/src/main.jsx
+++ team1_react/src/main.jsx
@@ -1,18 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import Header from './layout/Header.jsx';
 import SideBar from './layout/SideBar.jsx';
 import Topline from './layout/Topline.jsx';
+import Storage from './components/layout/Storage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/*<App />*/}
     <Header />
     <div className="d-flex">
-    <SideBar />
-    <Topline/>
+      <SideBar />
+      {/*<Topline/>*/}
+      <Storage />
     </div>
   </StrictMode>,
-)
+);
