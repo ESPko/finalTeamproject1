a455717ca5c6d7ee03f6cdeb3de0875d06bf73f9
jungg <yjungggg@gmail.com>
2025-05-02 16:38:53 +0900

    사이드바 수정

--- team1_react/src/theme/Side2.jsx
+++ team1_react/src/theme/Side2.jsx
@@ -1,5 +1,4 @@
 import { Link, Outlet, useLocation } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 
 const menuItems = [
   { label: '재고', path: '/stock' },
@@ -12,23 +11,6 @@ const menuItems = [
 
 function Side2() {
   const location = useLocation();
-  const [isDark, setIsDark] = useState(false);
-
-  // 페이지 로드 시, 저장된 테마를 불러옵니다.
-  useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
-    const isDarkMode = storedTheme === 'dark';
-    setIsDark(isDarkMode);
-    document.documentElement.classList.toggle('dark', isDarkMode); // 다크모드 적용
-  }, []);
-
-  // 테마를 토글하는 함수
-  const toggleTheme = () => {
-    const nextTheme = !isDark;
-    setIsDark(nextTheme);
-    document.documentElement.classList.toggle('dark', nextTheme); // 다크모드 적용
-    localStorage.setItem('theme', nextTheme ? 'dark' : 'light'); // 로컬스토리지에 상태 저장
-  };
 
   return (
     <div className="flex flex-1">
@@ -62,17 +44,6 @@ function Side2() {
             })}
           </ul>
         </nav>
-
-        {/* Dark/Light Mode Toggle */}
-        <div className="flex p-5">
-          <span className="text-sm text-gray-600 dark:text-white mr-2">{isDark ? '다크 모드' : '라이트 모드'}</span>
-          <button
-            onClick={toggleTheme}
-            className={`w-10 h-5 rounded-full p-1 flex items-center justify-${isDark ? 'end' : 'start'} bg-gray-300 dark:bg-gray-600`}
-          >
-            <div className={`w-4 h-4 bg-white rounded-full shadow transform transition-transform duration-300`} />
-          </button>
-        </div>
       </aside>
 
       {/* Content */}
