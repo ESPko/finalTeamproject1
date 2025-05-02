72f56c8bdcc0e4529eaae415e6536c5cde392681
CodeMinSeo <rnrmf6076@gmail.com>
2025-05-02 15:53:02 +0900

    jwt_시도전_커밋

--- team1_react/src/page/TestPage1.jsx
+++ team1_react/src/page/TestPage1.jsx
@@ -1,9 +1,7 @@
 function TestPage1() {
   return (
     <main className=" flex-1 p-6 overflow-y-auto">
-      <div className="bg-white rounded shadow p-4 w-100 h-full">
-        <div className="text-gray-700 font-semibold mb-2">Test1</div>
-      </div>
+      main
     </main>
   );
 }
