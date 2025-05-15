f3c0188618e05e5ef1d8b6c0624dd133ee292e18
jungg <yjungggg@gmail.com>
2025-05-15 16:40:25 +0900

    헤더 알림 아이콘 삭제

--- team1_react/src/components/HeaderIcons2.jsx
+++ team1_react/src/components/HeaderIcons2.jsx
@@ -19,13 +19,13 @@ const HeaderIcons2 = ({ onToggleTheme }) => {
       </button>
 
       {/* Bell 알림 아이콘 */}
-      <div className="relative w-10 h-10 border border-black/30 rounded-md flex items-center justify-center cursor-pointer
-                      hover:bg-black/3 transition-colors duration-300">
-        <Bell size={20} className="text-black" />
-        <span className="absolute -top-2 -right-2 text-[10px] text-black font-bold bg-[#f4b63d] w-5 h-5 flex items-center justify-center rounded-full">
-          7
-        </span>
-      </div>
+      {/*<div className="relative w-10 h-10 border border-black/30 rounded-md flex items-center justify-center cursor-pointer*/}
+      {/*                hover:bg-black/3 transition-colors duration-300">*/}
+      {/*  <Bell size={20} className="text-black" />*/}
+      {/*  <span className="absolute -top-2 -right-2 text-[10px] text-black font-bold bg-[#f4b63d] w-5 h-5 flex items-center justify-center rounded-full">*/}
+      {/*    7*/}
+      {/*  </span>*/}
+      {/*</div>*/}
     </div>
   );
 };
