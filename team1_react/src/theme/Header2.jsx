cff1a3f9cb857c404ca6b215a6d5a4b800a9a0ef
CodeMinSeo <rnrmf6076@gmail.com>
2025-05-16 11:06:52 +0900

    no message

--- team1_react/src/theme/Header2.jsx
+++ team1_react/src/theme/Header2.jsx
@@ -2,32 +2,23 @@ import { Link } from 'react-router-dom';
 import HeaderIcons2 from '../components/HeaderIcons2.jsx';
 import { useAuth } from '../contexts/AuthContext.jsx';
 import { FiUser } from '@react-icons/all-files/fi/FiUser.js';
-import { useState } from 'react';
 
-function Header2({ toggleLayout }) {
-  const { user } = useAuth();
-  const [token, setToken] = useState(localStorage.getItem('token') || '');
+function Header2({toggleLayout }) {
+  const { user, token, logout, loading } = useAuth(); // AuthContext에서 token, logout 받아옴
 
-  // 로그아웃 처리
-  const handleLogout = () => {
-    setToken('');
-    localStorage.removeItem('token');
-  };
+  if (loading) {
+    return "로딩중...";
+  }
 
   return (
-    <header
-      className="flex items-center justify-between border-b border-gray-100 bg-white h-16 px-6"
-      style={{ height: '64px', minHeight: '64px', maxHeight: '64px', overflow: 'hidden' }}>
+    <header className="flex items-center justify-between border-b border-gray-100 bg-[#ffffff] h-16 px-6">
       <Link to="/" className="!text-[#9379db] text-2xl font-bold tracking-wide">
         JAEGOHAE
       </Link>
-
-
       <div className="flex items-center space-x-4">
         <HeaderIcons2 onToggleTheme={toggleLayout} />
 
-        <div
-          className="flex items-center space-x-2 backdrop-blur bg-white/5 px-3 py-1.5 rounded-full text-white text-sm font-medium shadow hover:bg-black/3 transition-colors duration-300">
+        <div className="flex items-center space-x-2 backdrop-blur bg-white/5 px-3 py-1.5 rounded-full text-white text-sm font-medium shadow hover:bg-black/3 transition-colors duration-300">
           <FiUser
             className={`${
               user?.position === 0
@@ -42,20 +33,22 @@ function Header2({ toggleLayout }) {
           <span className="tracking-wide text-black">{user?.id}</span>
         </div>
 
-        {token ?
-          <Link
-            to="/login"
-            className="bg-[#a599ed] text-white text-sm px-4 py-1 rounded hover:bg-[#8e7dde]"
-            onClick={handleLogout}
+        {/* 로그인 / 로그아웃 버튼 */}
+        {token ? (
+          <button
+            onClick={logout}
+            className="bg-[#a599ed] text-white text-sm px-4 py-1 rounded hover:bg-[#a599ed]"
           >
             Logout
-          </Link>
-          : <Link
+          </button>
+        ) : (
+          <Link
             to="/login"
-            className="bg-[#a599ed] text-white text-sm px-4 py-1 rounded hover:bg-[#8e7dde]"
+            className="bg-[#a599ed] text-white text-sm px-4 py-1 rounded hover:bg-[#a599ed]"
           >
             Login
-          </Link>}
+          </Link>
+        )}
       </div>
     </header>
   );
