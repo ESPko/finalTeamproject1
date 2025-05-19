138c9e336562753cbb0eed2f9bd71426fe0cc2c2
ESPko <asddsa362@naver.com>
2025-05-19 16:06:56 +0900

    no message

--- team1_react/src/page/main/MainPage.jsx
+++ team1_react/src/page/main/MainPage.jsx
@@ -5,6 +5,7 @@ import DashboardChart from './DashboardChart.jsx';
 import MainPageInputRequestList from './MainPageInputRequestList.jsx';
 import MainLowStockPage from './MainLowStockPage.jsx';
 import { useNavigate } from 'react-router-dom';
+import StockMovementChart from './StockMovementChart.jsx';
 
 
 const ioData = [
@@ -37,52 +38,9 @@ const Dashboard = () =>{
         <MainLowStockPage />
       </div>
 
-
       {/* 입출고 수 */}
-      <div className="bg-white rounded-2xl shadow p-4  h-[420px]">
-        <div className="flex justify-between items-center mb-4">
-          <div className="text-lg font-bold text-gray-800">한달 기준 입출고 수</div>
-          <div className="text-sm text-gray-400">2025-04-01 ~ 2025-04-30</div>
-        </div>
-        <div className="h-56">
-          <ResponsiveLine
-            data={[
-              {
-                id: "입고",
-                data: ioData.map(d => ({ x: d.date, y: d.입고 })),
-              },
-              {
-                id: "출고",
-                data: ioData.map(d => ({ x: d.date, y: d.출고 })),
-              },
-            ]}
-            margin={{ top: 20, right: 20, bottom: 40, left: 40 }}
-            xScale={{ type: "point" }}
-            yScale={{ type: "linear", stacked: true, min: 0, max: "auto" }}
-            axisBottom={{
-              tickSize: 5,
-              tickPadding: 5,
-              legend: "날짜",
-              legendPosition: "middle",
-              legendOffset: 32,
-            }}
-            axisLeft={{
-              tickSize: 5,
-              tickPadding: 5,
-              legend: "수량",
-              legendPosition: "middle",
-              legendOffset: -40,
-            }}
-            colors={["#60A5FA", "#F87171"]} // 입고-파랑, 출고-빨강
-            pointSize={8}
-            pointBorderWidth={2}
-            pointBorderColor={{ from: "serieColor" }}
-            useMesh={true}
-            enableArea={true} // 영역 강조
-            areaOpacity={0.4}
-          />
-
-        </div>
+      <div className="bg-white rounded-2xl shadow p-4  h-[400px] hover:scale-105 hover:shadow-lg hover:cursor-pointer" onClick={() => navigate('/test9')}>
+        <StockMovementChart/>
       </div>
     </main>
   );
